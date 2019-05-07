89c1cbcf368ca52ed8241423b9f3365f27cf021d
SophiaRakhis <sophiarakhis@gmail.com>
2019-05-07 16:07:07 +0200

    Exercice02

--- js.js
+++ js.js
@@ -1,9 +1,8 @@
-var numerique= 3
-var chaine_de_caractere= "Bonjour, aurevoir merci."
-var vrai = true
-var je_suis_faux = false
-
-console.log(typeof(3));
-console.log(typeof ("Bonjour, aurevoir merci."));
-console.log(typeof(true));
-console.log(typeof(false));
+const valeurRandom = Math.random() > 0.5;
+    if (valeurRandom==true)
+    {
+        console.log("c'est vrai");
+    }
+    else {
+        console.log("c'est faux");
+    }
