b4ed10babc1e5d36f4b0edb619f33a1a8c6afbe7
SophiaRakhis <sophiarakhis@gmail.com>
2019-05-07 15:13:58 +0200

    Exercice 01

---
+++ js.js
@@ -0,0 +1,9 @@
+var numerique= 3
+var chaine_de_caractere= "Bonjour, aurevoir merci."
+var vrai = true
+var je_suis_faux = false
+
+console.log(typeof(3));
+console.log(typeof ("Bonjour, aurevoir merci."));
+console.log(typeof(true));
+console.log(typeof(false));
